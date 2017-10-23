8250454ad745d14f88afdf40aa509196c6aff909
ganesh3s3 <miffychitu@gmail.com>
2017-10-23 16:06:24 +0530

    friendpicker populates from friendstable(online)

--- app/src/main/java/com/ecksday/borrowtracker/BLViewFragment.java
+++ app/src/main/java/com/ecksday/borrowtracker/BLViewFragment.java
@@ -10,25 +10,42 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+
 /**
  * Created by G551JK-DM053H on 13-10-2017.
  */
 
 public class BLViewFragment extends Fragment implements TransactionDialogFragment.TransactionDialogListener {
 
-    TextView OweTextView, OweNumView;
+    RequestQueue requestQueue;
+    TextView POY_Text_View, POY_Num_View, YOP_Text_View, YOP_Num_View;
     Button GiveButton, CollectButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View RootView = inflater.inflate(R.layout.fragment_b_l_view, container, false);
 
-        OweTextView = (TextView)RootView.findViewById(R.id.owe_text_view);
-        OweNumView = (TextView)RootView.findViewById(R.id.owe_num_view);
+        POY_Text_View = (TextView)RootView.findViewById(R.id.poy_text_view);
+        POY_Num_View = (TextView)RootView.findViewById(R.id.poy_num_view);
+        YOP_Text_View = (TextView)RootView.findViewById(R.id.yop_text_view);
+        YOP_Num_View = (TextView)RootView.findViewById(R.id.yop_num_view);
         GiveButton = (Button)RootView.findViewById(R.id.give_button);
         CollectButton = (Button)RootView.findViewById(R.id.collect_button);
 
+        POY_Text_View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+        YOP_Text_View.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         GiveButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,18 +69,94 @@ public class BLViewFragment extends Fragment implements TransactionDialogFragmen
     }
 
     @Override
-    public void onDialogPositiveClick(DialogFragment dialog, String friendName, float transactionAmount) {
+    public void onDialogPositiveClick(DialogFragment dialog, String friend_id, float transactionAmount) {
+        /*requestQueue = Volley.newRequestQueue(getActivity());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpRetrieveFriendsUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String stringResponse) {
+                if(stringResponse.equals("No friends. FeelsBadMan :gun:")){
+                    Snackbar snackbar = Snackbar
+                            .make(getView(), stringResponse, Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                }
+                else {
+                    try {
+
+                    }
+                    catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse errorRes = error.networkResponse;
+                String stringData = "";
+                try{
+                    if(errorRes != null && errorRes.data != null){
+                        stringData = new String(errorRes.data,"UTF-8");
+                    }}
+                catch (UnsupportedEncodingException e){
+
+                }
+                Log.e("Error",stringData);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> parameters = new HashMap<String, String>();
+                parameters.put("user_id", User_Id_Holder);
+                return parameters;
+            }
+        };
+
+        requestQueue.add(stringRequest);*/
+
 
         // User touched the dialog's positive button
-        float owedAmount = Float.parseFloat(OweNumView.getText().toString());
         if(dialog.getTag().equals("give")){
-            owedAmount +=transactionAmount;
-        }
-        else if(dialog.getTag().equals("collect")){
-            owedAmount -=transactionAmount;
+            float owedAmount = Float.parseFloat(YOP_Num_View.getText().toString());
+            if(owedAmount>0f) {
+                owedAmount -= transactionAmount;
+                if(owedAmount>=0) {
+                    setYOP(owedAmount);
+                }
+                else {
+                    setYOP(0f);
+                    setPOY(Math.abs(owedAmount));
+                }
+            }
+            else {
+                owedAmount = Float.parseFloat(POY_Num_View.getText().toString());
+                owedAmount+=transactionAmount;
+                setPOY(owedAmount);
+            }
         }
 
-        OweNumView.setText(String.valueOf(owedAmount));
+        else if(dialog.getTag().equals("collect")){
+            float owedAmount = Float.parseFloat(POY_Num_View.getText().toString());
+            if(owedAmount>0f) {
+                owedAmount -= transactionAmount;
+                if(owedAmount>=0) {
+                    setPOY(owedAmount);
+                }
+                else {
+                    setPOY(0f);
+                    setYOP(Math.abs(owedAmount));
+                }
+            }
+            else {
+                owedAmount = Float.parseFloat(YOP_Num_View.getText().toString());
+                owedAmount+=transactionAmount;
+                setYOP(owedAmount);
+            }
+        }
+
+
 
     }
 
@@ -71,6 +164,15 @@ public class BLViewFragment extends Fragment implements TransactionDialogFragmen
     public void onDialogNegativeClick(DialogFragment dialog) {
         // User touched the dialog's negative button
         dialog.dismiss();
+    }
+
+    private void setPOY(float owedAmount) {
+        POY_Num_View.setText(String.valueOf(owedAmount));
+    }
+
+    private void setYOP(float owedAmount){
+
+        YOP_Num_View.setText(String.valueOf(owedAmount));
     }
 
 }
--- app/src/main/java/com/ecksday/borrowtracker/TransactionDialogFragment.java
+++ app/src/main/java/com/ecksday/borrowtracker/TransactionDialogFragment.java
@@ -3,20 +3,44 @@ package com.ecksday.borrowtracker;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.text.InputFilter;
 import android.text.Spanned;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import fr.ganfra.materialspinner.MaterialSpinner;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by G551JK-DM053H on 13-10-2017.
@@ -24,8 +48,17 @@ import fr.ganfra.materialspinner.MaterialSpinner;
 
 public class TransactionDialogFragment extends DialogFragment {
 
+    RequestQueue requestQueue;
+    String HttpRetrieveFriendsUrl = "http://ecksday.com/btadmin/RetrieveFriends.php";
+    List<String> friendsList = new ArrayList<>();
+    private HashMap<String, String> hmapcat = new HashMap<>();
+    String friend_id;
+
+    SharedPreferences sharedPreferences;
+    String User_Id_Holder;
+
     public interface TransactionDialogListener {
-        public void onDialogPositiveClick(DialogFragment dialog, String friendName, float transactionAmount);
+        public void onDialogPositiveClick(DialogFragment dialog, String FriendId, float transactionAmount);
         public void onDialogNegativeClick(DialogFragment dialog);
     }
 
@@ -38,17 +71,92 @@ public class TransactionDialogFragment extends DialogFragment {
         // Get the layout inflater
         LayoutInflater inflater = getActivity().getLayoutInflater();
 
-        View RootView = inflater.inflate(R.layout.fragment_transaction_dialog, null);
+        final View RootView = inflater.inflate(R.layout.fragment_transaction_dialog, null);
+        requestQueue = Volley.newRequestQueue(getActivity());
+        sharedPreferences = getActivity().getSharedPreferences("logindetails",MODE_PRIVATE);
+        User_Id_Holder= sharedPreferences.getString("user_id","");
 
-        String[] friendArray = {"Mani", "Ananthi", "Devaki"};
+        final Spinner friendPicker = (MaterialSpinner)RootView.findViewById(R.id.friend_picker);
+        friendPicker.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
-        final Spinner locationSpinner = (MaterialSpinner)RootView.findViewById(R.id.friend_picker);
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, friendArray);
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+                String friend_full_name = friendPicker.getSelectedItem().toString();
+                Log.i("Selected item : ", friend_full_name);
+                friend_id=hmapcat.get(friend_full_name );  // here you will get ids
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+
+            }
+
+        });
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, friendsList);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        locationSpinner.setAdapter(dataAdapter);
+        friendPicker.setAdapter(dataAdapter);
 
         final EditText TransactionAmount = (EditText) RootView.findViewById(R.id.transaction_amount);
         TransactionAmount.setFilters(new InputFilter[] {new CurrencyFormatInputFilter()});
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, HttpRetrieveFriendsUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String stringResponse) {
+                if(stringResponse.equals("No friends. FeelsBadMan :gun:")){
+                    Snackbar snackbar = Snackbar
+                            .make(RootView, stringResponse, Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                    friendPicker.setEnabled(false);
+                    TransactionAmount.setEnabled(false);
+                }
+                else {
+                    try {
+                        JSONArray jFriends = new JSONArray(stringResponse);
+                        for (int i = 0; i < jFriends.length(); i++) {
+                            JSONObject jFriend = jFriends.getJSONObject(i);
+                            String friend_id = jFriend.getString("user_id_2");
+                            String friend_full_name = jFriend.getString(("friend_full_name"));
+                            friendsList.add(friend_full_name);
+                            hmapcat.put(friend_full_name, friend_id);
+                        }
+                    }
+                    catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse errorRes = error.networkResponse;
+                String stringData = "";
+                try{
+                    if(errorRes != null && errorRes.data != null){
+                        stringData = new String(errorRes.data,"UTF-8");
+                    }}
+                catch (UnsupportedEncodingException e){
+
+                }
+                Log.e("Error",stringData);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> parameters = new HashMap<String, String>();
+                parameters.put("user_id", User_Id_Holder);
+                return parameters;
+            }
+        };
+
+        requestQueue.add(stringRequest);
+
+
+
+
 
 
         builder.setMessage("Enter Details")
@@ -58,7 +166,7 @@ public class TransactionDialogFragment extends DialogFragment {
                     @Override
                     public void onClick(DialogInterface dialog, int id) {
                         final float transactionAmount = Float.parseFloat(TransactionAmount.getText().toString());
-                        ((TransactionDialogListener)getTargetFragment()).onDialogPositiveClick(TransactionDialogFragment.this, locationSpinner.getSelectedItem().toString(), transactionAmount);
+                        ((TransactionDialogListener)getTargetFragment()).onDialogPositiveClick(TransactionDialogFragment.this,friend_id, transactionAmount);
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
